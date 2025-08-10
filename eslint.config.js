1a0f7ffbdd253763b9de643dffaa49a60b6913ed
ykob <info@tplh.net>
2025-08-10 22:02:09 +0900

    update eslint and prettier configs.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,11 @@
-import prettierConfig from 'eslint-config-prettier';
-import eslintPluginAstro from 'eslint-plugin-astro';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
-export default [...eslintPluginAstro.configs.recommended, prettierConfig];
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+    },
+  },
+});
