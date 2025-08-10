7ea83368ded9829b384c7e735a9e8b8d45abc512
ykob <info@tplh.net>
2025-08-10 21:42:19 +0900

    update eslint configs.

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,4 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
 import prettierConfig from 'eslint-config-prettier';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  prettierConfig,
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx}'],
-    plugins: {
-      react: reactPlugin,
-    },
-    rules: {
-      ...reactPlugin.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
-);
+export default [...eslintPluginAstro.configs.recommended, prettierConfig];
