564d0f3b1244b78e814cc87dd020f2057b127b6f
ykob <info@tplh.net>
2025-08-13 22:17:45 +0900

    remove eslint-plugin-astro.

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,11 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
-import eslintPluginAstro from 'eslint-plugin-astro';
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
     },
-  }
-);
+  },
+});
