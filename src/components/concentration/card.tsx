84b5453d6dc393e2f86c993b5932530184e8f443
ykob <info@tplh.net>
2025-08-19 23:47:47 +0900

    add react component on game content.

---
+++ src/components/concentration/card.tsx
@@ -0,0 +1,11 @@
+import { css } from 'styled-system/css';
+
+export const Card = () => {
+  return <div className={styles.container}>Card</div>;
+};
+
+const styles = {
+  container: css({
+    aspectRatio: '',
+  }),
+};
---
+++ src/components/concentration/concentration-content.tsx
@@ -0,0 +1,3 @@
+export const ConcentrationContent = () => {
+  return <div>ConcentrationContent</div>;
+};
