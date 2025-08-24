5b1f89235500f41ac82de5aca0d98f79162302a4
ykob <info@tplh.net>
2025-08-24 23:48:11 +0900

    update astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
