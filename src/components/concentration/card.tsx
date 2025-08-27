9922c9e20d8e0bfbb8faba650df493998c56fab2
ykob <info@tplh.net>
2025-08-27 22:49:58 +0900

    added card flip animation.

--- src/components/concentration/card.tsx
+++ src/components/concentration/card.tsx
@@ -1,15 +1,85 @@
 import { css } from 'styled-system/css';
+import { cva } from 'styled-system/css/cva.mjs';
 
-export const Card = () => {
-  return <div className={styles.container}>Card</div>;
+type CardProps = {
+  flipped: boolean;
+  disabled: boolean;
+  onClick: () => void;
+};
+
+export const Card = ({ flipped, disabled, onClick }: CardProps) => {
+  return (
+    <button onClick={onClick}>
+      <div className={styles.container}>
+        <div
+          className={styles.front({
+            flipped,
+            disabled,
+          })}
+        >
+          表
+        </div>
+        <div
+          className={styles.back({
+            flipped,
+            disabled,
+          })}
+        >
+          裏
+        </div>
+      </div>
+    </button>
+  );
 };
 
 const styles = {
   container: css({
     cursor: 'pointer',
-    rounded: '4%',
-    overflow: 'hidden',
+    position: 'relative',
     aspectRatio: '2.5 / 3.5',
-    bgColor: '#ccc',
+    perspective: '1000px',
+  }),
+  front: cva({
+    base: {
+      position: 'absolute',
+      inset: '0',
+      rounded: '4%',
+      bgColor: '#ccc',
+      backfaceVisibility: 'hidden',
+      transform: 'rotate3d(0, 1, 0, 0deg)',
+      transition: 'transform 0.2s',
+    },
+    variants: {
+      flipped: {
+        true: {
+          transform: 'rotate3d(0, 1, 0, 180deg)',
+        },
+        false: {
+          transform: 'rotate3d(0, 1, 0, 0deg)',
+        },
+      },
+    },
+  }),
+  back: cva({
+    base: {
+      position: 'absolute',
+      inset: '0',
+      rounded: '4%',
+      color: '#fff',
+      bgColor: '#000',
+      backfaceVisibility: 'hidden',
+      transform: 'rotate3d(0, 1, 0, 180deg)',
+      transition: 'transform 0.2s',
+    },
+    variants: {
+      flipped: {
+        true: {
+          transform: 'rotate3d(0, 1, 0, 0deg)',
+        },
+        false: {
+          transform: 'rotate3d(0, 1, 0, 180deg)',
+        },
+      },
+    },
   }),
 };
--- src/components/concentration/content.tsx
+++ src/components/concentration/content.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import { css } from 'styled-system/css';
 import { Card } from './card';
 
+const baseCards = Array.from({ length: 12 }, () => {
+  return {
+    id: Math.random(),
+    flipped: false,
+    disabled: false,
+  };
+});
+
 export const Content = () => {
+  const [cards, setCards] = useState(baseCards);
+
   return (
     <div className={styles.container}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {cards.map((card) => {
+        return (
+          <Card
+            key={card.id}
+            flipped={card.flipped}
+            disabled={card.disabled}
+            onClick={() => {
+              setCards((prevCards) => {
+                return prevCards.map((prevCard) => {
+                  if (prevCard.id === card.id) {
+                    return {
+                      ...prevCard,
+                      flipped: !prevCard.flipped,
+                    };
+                  }
+                  return prevCard;
+                });
+              });
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
@@ -25,5 +46,6 @@ const styles = {
     display: 'grid',
     gridTemplateColumns: 'repeat(4, 1fr)',
     gap: '8px',
+    perspective: '100px',
   }),
 };
