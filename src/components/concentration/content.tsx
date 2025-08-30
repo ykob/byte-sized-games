c4ecd45355a8b5a441d01ed166394d9cfad47fa5
ykob <info@tplh.net>
2025-08-30 23:52:44 +0900

    added the basis effect for concentration.

--- src/components/concentration/content.tsx
+++ src/components/concentration/content.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { css } from 'styled-system/css';
 import { Card } from './card';
 
@@ -26,12 +26,23 @@ const baseCards: Card[] = Array.from({ length: 12 }, (_, i) => {
 
 export const Content = () => {
   const [cards, setCards] = useState(shuffleCards(baseCards));
+  const selectNumbers = useRef<[number, number]>([-1, -1]);
+  const collectedNumbers = useRef<number[]>([]);
 
   const onClickCard = useCallback(
     (id: number) => {
+      const thisCard = cards.find((card) => card.id === id);
+
+      if (!thisCard || thisCard.flipped || selectNumbers.current[1] > -1) {
+        return;
+      }
+      selectNumbers.current =
+        selectNumbers.current[0] === -1
+          ? [thisCard.number, -1]
+          : [selectNumbers.current[0], thisCard.number];
       setCards((prevCards) => {
         return prevCards.map((prevCard) => {
-          if (prevCard.id === id && !prevCard.flipped) {
+          if (prevCard.id === id) {
             return {
               ...prevCard,
               flipped: !prevCard.flipped,
@@ -43,6 +54,28 @@ export const Content = () => {
     },
     [cards]
   );
+
+  useEffect(() => {
+    if (selectNumbers.current[0] === -1 || selectNumbers.current[1] === -1) {
+      return;
+    }
+    if (selectNumbers.current[0] === selectNumbers.current[1]) {
+      collectedNumbers.current.push(selectNumbers.current[0]);
+      selectNumbers.current = [-1, -1];
+    } else {
+      setTimeout(() => {
+        selectNumbers.current = [-1, -1];
+        setCards((prevCards) => {
+          return prevCards.map((prevCard) => {
+            return {
+              ...prevCard,
+              flipped: collectedNumbers.current.includes(prevCard.number),
+            };
+          });
+        });
+      }, 500);
+    }
+  }, [selectNumbers.current]);
 
   return (
     <div className={styles.container}>
