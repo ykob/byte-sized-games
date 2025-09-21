8469d106852b3fe2998ba0e656125e75afff4de5
ykob <info@tplh.net>
2025-09-21 13:01:32 +0900

    added semantic colors token.

--- panda/semantic-tokens/colors.ts
+++ panda/semantic-tokens/colors.ts
@@ -1,3 +1,17 @@
 import type { Recursive, SemanticToken } from '@pandacss/types';
 
-export const colors: Recursive<SemanticToken<string, string>> = {};
+export const colors: Recursive<SemanticToken<string, string>> = {
+  text: {
+    DEFAULT: { value: '{colors.gray.950}' },
+    secondary: { value: '{colors.gray.700}' },
+  },
+  bg: {
+    DEFAULT: { value: '{colors.teal.50}' },
+    secondary: { value: '{colors.teal.100}' },
+  },
+  button: {
+    text: { value: 'white' },
+    bg: { value: '{colors.teal.500}' },
+    bevel: { value: '{colors.teal.700}' },
+  },
+};
