8d12c28ed7c59461285c7024f9ead47e8aa18751
ykob <info@tplh.net>
2025-09-25 21:53:33 +0900

    added button type variant.

--- panda/semantic-tokens/colors.ts
+++ panda/semantic-tokens/colors.ts
@@ -10,8 +10,25 @@ export const colors: Recursive<SemanticToken<string, string>> = {
     secondary: { value: '{colors.teal.100}' },
   },
   button: {
-    text: { value: 'white' },
-    bg: { value: '{colors.teal.500}' },
-    bevel: { value: '{colors.teal.700}' },
+    primary: {
+      text: { value: 'white' },
+      bg: { value: '{colors.teal.500}' },
+      bevel: { value: '{colors.teal.700}' },
+    },
+    secondary: {
+      text: { value: 'white' },
+      bg: { value: '{colors.gray.800}' },
+      bevel: { value: '{colors.gray.950}' },
+    },
+    danger: {
+      text: { value: 'white' },
+      bg: { value: '{colors.violet.500}' },
+      bevel: { value: '{colors.violet.700}' },
+    },
+    disabled: {
+      text: { value: '{colors.gray.400}' },
+      bg: { value: '{colors.gray.200}' },
+      bevel: { value: '{colors.gray.300}' },
+    },
   },
 };
--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,25 +1,53 @@
 import type { ComponentProps } from 'react';
-import { css, cx } from 'styled-system/css';
+import { cva, cx } from 'styled-system/css';
 
-type ButtonProps = ComponentProps<'button'>;
+type ButtonProps = ComponentProps<'button'> & {
+  buttonType: 'primary' | 'secondary' | 'danger';
+};
 
-export const Button = ({ children, className, ...props }: ButtonProps) => {
+export const Button = ({ buttonType = 'primary', children, className, ...props }: ButtonProps) => {
   return (
-    <button className={cx(styles.container, className)} {...props}>
+    <button
+      className={cx(
+        styles.container({
+          buttonType,
+        }),
+        className
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
 };
 
 const styles = {
-  container: css({
-    h: '2.4em',
-    px: '1.2em',
-    cursor: 'pointer',
-    rounded: '9999px',
-    fontSize: '1.5rem',
-    color: 'button.text',
-    bgColor: 'button.bg',
-    boxShadow: '0 3px 0 0 token(colors.button.bevel)',
+  container: cva({
+    base: {
+      h: '2.4em',
+      px: '1.2em',
+      cursor: 'pointer',
+      rounded: '9999px',
+      fontSize: '1.5rem',
+    },
+    variants: {
+      buttonType: {
+        primary: {
+          color: 'button.primary.text',
+          bgColor: 'button.primary.bg',
+          boxShadow: '0 3px 0 0 token(colors.button.primary.bevel)',
+        },
+        secondary: {
+          color: 'button.secondary.text',
+          bgColor: 'button.secondary.bg',
+          boxShadow: '0 3px 0 0 token(colors.button.secondary.bevel)',
+        },
+        danger: {
+          color: 'button.danger.text',
+          bgColor: 'button.danger.bg',
+          boxShadow: '0 3px 0 0 token(colors.button.danger.bevel)',
+        },
+      },
+    },
   }),
 };
