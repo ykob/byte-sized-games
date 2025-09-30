cc3347a91fb7e05210312a89a26161aa5c9c9ebf
ykob <info@tplh.net>
2025-09-30 22:19:19 +0900

    update timer styles.

--- src/components/common/timer.tsx
+++ src/components/common/timer.tsx
@@ -1,7 +1,28 @@
+import { mdiTimer } from '@mdi/js';
+import Icon from '@mdi/react';
+import { css } from 'styled-system/css';
+
 type TimerProps = {
   time: number;
 };
 
 export const Timer = ({ time }: TimerProps) => {
-  return <div>time: {Math.ceil(time / 1000)}</div>;
+  return (
+    <div className={styles.container}>
+      <Icon path={mdiTimer} size={1.5} />
+      <div className={styles.label}>{Math.ceil(time / 10)}</div>
+    </div>
+  );
+};
+
+const styles = {
+  container: css({
+    display: 'flex',
+    alignItems: 'center',
+  }),
+  label: css({
+    fontSize: '2xl',
+    fontWeight: '500',
+    lineHeight: 1,
+  }),
 };
