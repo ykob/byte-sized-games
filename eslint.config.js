42830bc9fb24eee3f1cabe1bb2a206e03ad8e110
ykob <info@tplh.net>
2025-10-11 22:43:48 +0900

    update the way to set eslint ignores.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  languageOptions: {
-    globals: {
-      ...globals.browser,
+export default [
+  ...tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
     },
-  },
-});
+  }),
+  globalIgnores(['.astro/*', 'dist/*', 'node_modules/*', 'styled-system/*']),
+];
