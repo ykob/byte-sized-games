feee373b29e97d4fe2069699af76863117c2084d
ykob <info@tplh.net>
2025-10-11 22:52:53 +0900

    update the way to set eslint ignores.

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,6 @@ export default [
     },
   }),
   {
-    ignores: ['.astro/*', 'dist/*', 'node_modules/*', 'styled-system/*'],
+    ignores: ['./.astro/*', './dist/*', './node_modules/*', './styled-system/*'],
   },
 ];
