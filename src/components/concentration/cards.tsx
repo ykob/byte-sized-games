ed9463e4ee0d95a417132fdffdbb7ef03d779ef7
ykob <info@tplh.net>
2025-10-25 16:31:05 +0900

    move the logic related to the card component.

--- src/components/concentration/cards.tsx
+++ src/components/concentration/cards.tsx
@@ -1,18 +1,75 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { css } from 'styled-system/css';
+import { shuffleArray } from '~/utils';
 import { Card } from './card';
 
-export type Card = {
+type Card = {
   id: number;
   number: number;
   flipped: boolean;
 };
 
-type CardsProps = {
-  cards: Card[];
-  onClickCard: (id: number) => void;
-};
+const baseCards: Card[] = Array.from({ length: 12 }, (_, i) => {
+  return {
+    id: Math.random(),
+    number: Math.floor(i / 2),
+    flipped: false,
+  };
+});
 
-export const Cards = ({ cards, onClickCard }: CardsProps) => {
+export const Cards = () => {
+  const selectNumbers = useRef<[number, number]>([-1, -1]);
+  const collectedNumbers = useRef<number[]>([]);
+  const [cards, setCards] = useState<Card[]>(shuffleArray(baseCards));
+
+  const onClickCard = useCallback(
+    (id: number) => {
+      const thisCard = cards.find((card) => card.id === id);
+
+      if (!thisCard || thisCard.flipped || selectNumbers.current[1] > -1) {
+        return;
+      }
+      selectNumbers.current =
+        selectNumbers.current[0] === -1
+          ? [thisCard.number, -1]
+          : [selectNumbers.current[0], thisCard.number];
+      setCards((prevCards) => {
+        return prevCards.map((prevCard) => {
+          if (prevCard.id === id) {
+            return {
+              ...prevCard,
+              flipped: !prevCard.flipped,
+            };
+          }
+          return prevCard;
+        });
+      });
+    },
+    [cards]
+  );
+
+  useEffect(() => {
+    if (selectNumbers.current[0] === -1 || selectNumbers.current[1] === -1) {
+      return;
+    }
+    if (selectNumbers.current[0] === selectNumbers.current[1]) {
+      collectedNumbers.current.push(selectNumbers.current[0]);
+      selectNumbers.current = [-1, -1];
+    } else {
+      setTimeout(() => {
+        selectNumbers.current = [-1, -1];
+        setCards((prevCards) => {
+          return prevCards.map((prevCard) => {
+            return {
+              ...prevCard,
+              flipped: collectedNumbers.current.includes(prevCard.number),
+            };
+          });
+        });
+      }, 500);
+    }
+  }, [selectNumbers.current]);
+
   return (
     <div className={styles.container}>
       {cards.map((card) => {
--- src/components/concentration/content.tsx
+++ src/components/concentration/content.tsx
@@ -1,77 +1,17 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { GameIntroduction } from '~/components/common/';
-import { shuffleArray } from '~/utils';
-import { Cards, type Card } from './cards';
-
-const baseCards: Card[] = Array.from({ length: 12 }, (_, i) => {
-  return {
-    id: Math.random(),
-    number: Math.floor(i / 2),
-    flipped: false,
-  };
-});
+import { Cards } from './cards';
 
 export const Content = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [cards, setCards] = useState<Card[]>([]);
-  const selectNumbers = useRef<[number, number]>([-1, -1]);
-  const collectedNumbers = useRef<number[]>([]);
 
-  const onClickCard = useCallback(
-    (id: number) => {
-      const thisCard = cards.find((card) => card.id === id);
-
-      if (!thisCard || thisCard.flipped || selectNumbers.current[1] > -1) {
-        return;
-      }
-      selectNumbers.current =
-        selectNumbers.current[0] === -1
-          ? [thisCard.number, -1]
-          : [selectNumbers.current[0], thisCard.number];
-      setCards((prevCards) => {
-        return prevCards.map((prevCard) => {
-          if (prevCard.id === id) {
-            return {
-              ...prevCard,
-              flipped: !prevCard.flipped,
-            };
-          }
-          return prevCard;
-        });
-      });
-    },
-    [cards]
-  );
   const startGame = () => {
     setIsPlaying(true);
-    setCards(shuffleArray(baseCards));
   };
-
-  useEffect(() => {
-    if (selectNumbers.current[0] === -1 || selectNumbers.current[1] === -1) {
-      return;
-    }
-    if (selectNumbers.current[0] === selectNumbers.current[1]) {
-      collectedNumbers.current.push(selectNumbers.current[0]);
-      selectNumbers.current = [-1, -1];
-    } else {
-      setTimeout(() => {
-        selectNumbers.current = [-1, -1];
-        setCards((prevCards) => {
-          return prevCards.map((prevCard) => {
-            return {
-              ...prevCard,
-              flipped: collectedNumbers.current.includes(prevCard.number),
-            };
-          });
-        });
-      }, 500);
-    }
-  }, [selectNumbers.current]);
 
   return (
     <div>
-      <Cards cards={cards} onClickCard={onClickCard} />
+      <Cards />
       {!isPlaying && <GameIntroduction title="Concentration" startGame={startGame} />}
     </div>
   );
