3dd1c4ac46621b89883271fab4f737152286141a
ykob <info@tplh.net>
2025-11-26 23:51:56 +0900

    update icon component style.

--- src/components/common/icon.tsx
+++ src/components/common/icon.tsx
@@ -1,3 +1,5 @@
+import { css } from 'styled-system/css';
+
 type IconProps = {
   path: string;
   size?: number;
@@ -7,8 +9,15 @@ export const Icon = ({ path, size = 1 }: IconProps) => {
   const width = 24 * size;
 
   return (
-    <svg width={width} height={width} viewBox="0 0 24 24">
+    <svg width={width} height={width} className={styles.container} viewBox="0 0 24 24">
       <path d={path} />
     </svg>
   );
+};
+
+const styles = {
+  container: css({
+    fill: 'currentColor',
+    display: 'inline-block',
+  }),
 };
