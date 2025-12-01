338d4d8c46c79b88898bffad71c43dcb567e304e
ykob <info@tplh.net>
2025-12-01 23:50:40 +0900

    update color token for button.

--- panda/semantic-tokens/colors.ts
+++ panda/semantic-tokens/colors.ts
@@ -13,16 +13,19 @@ export const colors: Recursive<SemanticToken<string, string>> = {
     primary: {
       text: { value: 'white' },
       bg: { value: '{colors.teal.500}' },
+      bgHover: { value: '{colors.teal.400}' },
       bevel: { value: '{colors.teal.700}' },
     },
     secondary: {
       text: { value: 'white' },
       bg: { value: '{colors.gray.800}' },
+      bgHover: { value: '{colors.gray.700}' },
       bevel: { value: '{colors.gray.950}' },
     },
     danger: {
       text: { value: 'white' },
       bg: { value: '{colors.violet.500}' },
+      bgHover: { value: '{colors.violet.400}' },
       bevel: { value: '{colors.violet.700}' },
     },
     disabled: {
--- src/components/common/button/styles.ts
+++ src/components/common/button/styles.ts
@@ -11,6 +11,7 @@ export const styles = {
       rounded: '9999px',
       fontSize: '2xl',
       letterSpacing: '0.04em',
+      transition: 'background-color 0.12s',
     },
     variants: {
       buttonType: {
@@ -18,16 +19,25 @@ export const styles = {
           color: 'button.primary.text',
           bgColor: 'button.primary.bg',
           boxShadow: '0 3px 0 0 token(colors.button.primary.bevel)',
+          _hover: {
+            bgColor: 'button.primary.bgHover',
+          },
         },
         secondary: {
           color: 'button.secondary.text',
           bgColor: 'button.secondary.bg',
           boxShadow: '0 3px 0 0 token(colors.button.secondary.bevel)',
+          _hover: {
+            bgColor: 'button.secondary.bgHover',
+          },
         },
         danger: {
           color: 'button.danger.text',
           bgColor: 'button.danger.bg',
           boxShadow: '0 3px 0 0 token(colors.button.danger.bevel)',
+          _hover: {
+            bgColor: 'button.danger.bgHover',
+          },
         },
       },
       circle: {
