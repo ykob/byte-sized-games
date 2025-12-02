1c3e0a25e71183f68a037c8ea1476e6afb8b44b8
ykob <info@tplh.net>
2025-12-02 23:47:17 +0900

    added icon button component.

---
+++ src/components/common/button/icon-button.tsx
@@ -0,0 +1,48 @@
+import type { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react';
+import { cx } from 'styled-system/css';
+import { Icon } from '../icon.tsx';
+import { styles } from './styles.ts';
+
+type BaseIconButtonProps = {
+  buttonType?: 'primary' | 'secondary' | 'danger';
+  circle?: boolean;
+  iconPath: string;
+};
+
+type IconButtonProps<C extends ElementType> = BaseIconButtonProps &
+  PropsWithChildren<{
+    as?: C;
+    className?: string;
+  }> &
+  Omit<
+    ComponentPropsWithoutRef<C>,
+    keyof (BaseIconButtonProps & { as?: C; children?: React.ReactNode; className?: string })
+  >;
+
+export const IconButton = <C extends ElementType = 'button'>({
+  as,
+  buttonType = 'primary',
+  children,
+  className,
+  circle = false,
+  iconPath,
+  ...props
+}: IconButtonProps<C>) => {
+  const Component = as || 'button';
+
+  return (
+    <Component
+      className={cx(
+        styles.container({
+          buttonType,
+          circle,
+        }),
+        className
+      )}
+      {...props}
+    >
+      <Icon path={iconPath} size={2} />
+      {children}
+    </Component>
+  );
+};
--- src/components/home/github-link.tsx
+++ src/components/home/github-link.tsx
@@ -1,16 +1,15 @@
 import { mdiGithub } from '@mdi/js';
-import { Button, Icon } from '../common';
+import { IconButton } from '../common';
 
 export const GithubLink = () => {
   return (
-    <Button
+    <IconButton
       as="a"
       circle
       href="https://github.com/ykob/byte-sized-games"
+      iconPath={mdiGithub}
       target="_blank"
       rel="noreferrer"
-    >
-      <Icon path={mdiGithub} size={2} />
-    </Button>
+    />
   );
 };
