d21b993ad4f753cf0558410bfd07a23d32b3a729
ykob <info@tplh.net>
2026-01-09 00:14:07 +0900

    added use puzzle drag hook.

--- src/components/jigsaw-puzzle/content.tsx
+++ src/components/jigsaw-puzzle/content.tsx
@@ -1,6 +1,7 @@
 import { useAtomValue, useSetAtom } from 'jotai';
 import { useEffect } from 'react';
 import { GameIntroduction, GameOver } from '~/components/common/';
+import { usePuzzleDrag } from '~/components/jigsaw-puzzle/hooks/use-puzzle-drag';
 import { useTimer } from '~/hooks';
 import { useIsTimerExpired } from '~/hooks/use-timer/store';
 import { PuzzleBoard } from './puzzle-board';
@@ -8,9 +9,7 @@ import {
   getGameOverAtom,
   getIsPlayingAtom,
   onGameOverAtom,
-  releasePieceAtom,
   resetGameAtom,
-  setCursorPositionAtom,
   startGameAtom,
 } from './stores';
 import { Timer } from './timer';
@@ -25,57 +24,24 @@ export const Content = () => {
   const gameOver = useAtomValue(getGameOverAtom);
   const isTimerExpired = useIsTimerExpired();
 
-  const releasePiece = useSetAtom(releasePieceAtom);
   const startGame = useSetAtom(startGameAtom);
   const resetGame = useSetAtom(resetGameAtom);
-  const setCursorPosition = useSetAtom(setCursorPositionAtom);
   const onGameOver = useSetAtom(onGameOverAtom);
 
-  const judgeFitPiece = (x: number, y: number) => {
-    const elements = document.elementsFromPoint(x, y);
-
-    if (elements.length === 0) {
-      return -1;
-    }
-
-    const element = elements.find((el) => el instanceof HTMLDivElement && el.dataset.pieceIndex);
-
-    if (element && element instanceof HTMLDivElement && element.dataset.pieceIndex) {
-      return parseInt(element.dataset.pieceIndex);
-    }
-    return -1;
-  };
-  const touchmove = (event: MouseEvent | TouchEvent) => {
-    const { clientX, clientY } = event instanceof MouseEvent ? event : event.touches[0];
-
-    setCursorPosition({ x: clientX, y: clientY });
-  };
-  const touchend = (event: MouseEvent | TouchEvent) => {
-    const { clientX, clientY } = event instanceof MouseEvent ? event : event.changedTouches[0];
-    const index = judgeFitPiece(clientX, clientY);
-
-    releasePiece(index);
-  };
-
-  useEffect(() => {
-    window.addEventListener('touchmove', touchmove);
-    window.addEventListener('mousemove', touchmove);
-    window.addEventListener('touchend', touchend);
-    window.addEventListener('mouseup', touchend);
-  }, []);
+  usePuzzleDrag();
 
   useEffect(() => {
     if (gameOver) {
       pauseTimer();
     }
-  }, [gameOver]);
+  }, [gameOver, pauseTimer]);
 
   useEffect(() => {
     if (isTimerExpired === true) {
       onGameOver();
       pauseTimer();
     }
-  }, [isTimerExpired]);
+  }, [isTimerExpired, onGameOver, pauseTimer]);
 
   return (
     <div>
---
+++ src/components/jigsaw-puzzle/hooks/use-puzzle-drag.ts
@@ -0,0 +1,49 @@
+import { useSetAtom } from 'jotai';
+import { useEffect } from 'react';
+import { releasePieceAtom, setCursorPositionAtom } from '~/components/jigsaw-puzzle/stores';
+
+export const usePuzzleDrag = () => {
+  const releasePiece = useSetAtom(releasePieceAtom);
+  const setCursorPosition = useSetAtom(setCursorPositionAtom);
+
+  const judgeFitPiece = (x: number, y: number) => {
+    const elements = document.elementsFromPoint(x, y);
+
+    if (elements.length === 0) {
+      return -1;
+    }
+
+    const element = elements.find((el) => el instanceof HTMLDivElement && el.dataset.pieceIndex);
+
+    if (element && element instanceof HTMLDivElement && element.dataset.pieceIndex) {
+      return parseInt(element.dataset.pieceIndex);
+    }
+    return -1;
+  };
+
+  useEffect(() => {
+    const touchmove = (event: MouseEvent | TouchEvent) => {
+      const { clientX, clientY } = event instanceof MouseEvent ? event : event.touches[0];
+
+      setCursorPosition({ x: clientX, y: clientY });
+    };
+    const touchend = (event: MouseEvent | TouchEvent) => {
+      const { clientX, clientY } = event instanceof MouseEvent ? event : event.changedTouches[0];
+      const index = judgeFitPiece(clientX, clientY);
+
+      releasePiece(index);
+    };
+
+    window.addEventListener('touchmove', touchmove);
+    window.addEventListener('mousemove', touchmove);
+    window.addEventListener('touchend', touchend);
+    window.addEventListener('mouseup', touchend);
+
+    return () => {
+      window.removeEventListener('touchmove', touchmove);
+      window.removeEventListener('mousemove', touchmove);
+      window.removeEventListener('touchend', touchend);
+      window.removeEventListener('mouseup', touchend);
+    };
+  }, [releasePiece, setCursorPosition]);
+};
