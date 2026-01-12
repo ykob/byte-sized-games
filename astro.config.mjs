4ce76af474ac533e72f9d30b202e0ec36b1f7550
ykob <info@tplh.net>
2026-01-13 00:13:12 +0900

    added deploy workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ykob.github.io',
+  base: '/byte-sized-games',
   integrations: [react()],
   server: {
     host: true,
