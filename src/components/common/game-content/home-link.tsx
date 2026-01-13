92d03f9b1d8b9121ca09535fdb652d62e8a3a741
ykob <info@tplh.net>
2026-01-13 23:11:33 +0900

    update home link.

--- src/components/common/game-content/home-link.tsx
+++ src/components/common/game-content/home-link.tsx
@@ -2,5 +2,13 @@ import { mdiHome } from '@mdi/js';
 import { IconButton } from '~/components/common';
 
 export const HomeLink = () => {
-  return <IconButton as="a" buttonType="secondary" circle href="/" iconPath={mdiHome} />;
+  return (
+    <IconButton
+      as="a"
+      buttonType="secondary"
+      circle
+      href={import.meta.env.BASE_URL}
+      iconPath={mdiHome}
+    />
+  );
 };
