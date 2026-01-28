340714c67e177305ff2052d8480952670586f9cd
ykob <info@tplh.net>
2026-01-28 23:32:33 +0900

    add eslint-plugin-react-hooks.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
   prettierConfig,
   {
     languageOptions: {
