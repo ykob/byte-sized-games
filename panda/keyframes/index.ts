d58ad37a719201205c2ed7d5b632883cab35b904
ykob <info@tplh.net>
2026-02-26 23:47:57 +0900

    added keyframes object for panda configs.

---
+++ panda/keyframes/index.ts
@@ -0,0 +1,20 @@
+import type { CssKeyframes } from '@pandacss/types';
+
+export const keyframes: CssKeyframes = {
+  explosionEffect: {
+    '0%': {
+      opacity: 0,
+      transform: 'scale(1)',
+    },
+    '10%': {
+      opacity: 1,
+    },
+    '80%': {
+      opacity: 1,
+    },
+    '100%': {
+      opacity: 0,
+      transform: 'scale(2)',
+    },
+  },
+};
