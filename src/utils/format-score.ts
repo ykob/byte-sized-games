8fb4a320b11e9ff5a8d3244d5e50761101a8bf18
ykob <info@tplh.net>
2026-03-03 21:11:49 +0900

    added zeroPadding on formatScore.

--- src/utils/format-score.ts
+++ src/utils/format-score.ts
@@ -1,6 +1,8 @@
+import { zeroPadding } from './zero-padding';
+
 export const SCORE_DISPLAY_LIMIT = 1000;
 export const MAX_SCORE_TEXT = '999+';
 
 export const formatScore = (score: number): string | number => {
-  return score < SCORE_DISPLAY_LIMIT ? score : MAX_SCORE_TEXT;
+  return score < SCORE_DISPLAY_LIMIT ? zeroPadding(score, 3) : MAX_SCORE_TEXT;
 };
