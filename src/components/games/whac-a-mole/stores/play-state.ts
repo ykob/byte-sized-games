0c9f278b3b395521399d13d91c74e92e9fdc250d
ykob <info@tplh.net>
2026-03-31 21:22:19 +0900

    added life atoms.

--- src/components/games/whac-a-mole/stores/play-state.ts
+++ src/components/games/whac-a-mole/stores/play-state.ts
@@ -1,16 +1,28 @@
 import { atom } from 'jotai';
+import { INITIAL_LIFE } from './constants';
 import { createMoles } from './create-moles';
 
 const molesAtom = atom(createMoles());
 const scoreAtom = atom(0);
+const lifeAtom = atom(INITIAL_LIFE);
 
 // Getter
 export const getMolesAtom = atom((get) => get(molesAtom));
 export const getScoreAtom = atom((get) => get(scoreAtom));
+export const getLifeAtom = atom((get) => get(lifeAtom));
 
 // Setter
+export const decrementLifeAtom = atom(null, (_, set) => {
+  set(lifeAtom, (prev) => Math.max(0, prev - 1));
+});
+
+export const resetLifeAtom = atom(null, (_, set) => {
+  set(lifeAtom, INITIAL_LIFE);
+});
+
 export const resetMolesAtom = atom(null, (_, set) => {
   set(scoreAtom, 0);
+  set(lifeAtom, INITIAL_LIFE);
   set(molesAtom, createMoles());
 });
 
@@ -21,7 +33,7 @@ export const hitGoodMoleAtom = atom(null, (get, set, index: number) => {
   if (!mole || mole.hit === true) return;
 
   mole.hit = true;
-  set(molesAtom, moles);
+  set(molesAtom, [...moles]);
   set(scoreAtom, (prev) => prev + 1);
 });
 
@@ -32,5 +44,6 @@ export const hitBadMoleAtom = atom(null, (get, set, index: number) => {
   if (!mole || mole.hit === true) return;
 
   mole.hit = true;
-  set(molesAtom, moles);
+  set(molesAtom, [...moles]);
+  set(decrementLifeAtom);
 });
