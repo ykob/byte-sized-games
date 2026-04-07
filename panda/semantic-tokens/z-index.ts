0659f5a49200a116de30859330fca56f6b73c1b1
ykob <info@tplh.net>
2026-04-07 19:25:18 +0900

    increased z-index tokens.

--- panda/semantic-tokens/z-index.ts
+++ panda/semantic-tokens/z-index.ts
@@ -3,8 +3,11 @@ import type { Recursive, SemanticToken } from '@pandacss/types';
 export const zIndex: Recursive<SemanticToken<string, string>> = {
   game: {
     base: { value: '{zIndex.0}' },
+    background: { value: '{zIndex.1}' },
     content: { value: '{zIndex.2}' },
-    ui: { value: '{zIndex.10}' },
+    foreground: { value: '{zIndex.3}' },
+    effect: { value: '{zIndex.10}' },
+    ui: { value: '{zIndex.100}' },
     overlay: { value: '{zIndex.9999}' },
   },
 };
--- panda/tokens/z-index.ts
+++ panda/tokens/z-index.ts
@@ -4,6 +4,8 @@ export const zIndex: Recursive<Token<string>> = {
   '0': { value: '0' },
   '1': { value: '1' },
   '2': { value: '2' },
+  '3': { value: '3' },
   '10': { value: '10' },
+  '100': { value: '100' },
   '9999': { value: '9999' },
 };
